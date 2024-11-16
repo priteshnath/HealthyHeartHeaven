34895be8b9771249a692d5d7cb9428555ff14691
Pritesh Nath <157404719+priteshnath@users.noreply.github.com>
2024-11-16 16:28:04 +0530

    Footer Updated

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -5,7 +5,7 @@ const Footer = () => {
     <footer className="relative bottom-0 w-full">
       <div className="footer-bottom w-full mx-auto py-6 px-8 border-t border-gray-400 bg-gray-300">
         <h2 className="text-2xl text-center">
-          Copyright © Healthy Heart Heaven 🧡 | Designed by Curious Coders
+          Copyright © Healthy Heart Heaven 🧡 | Designed by Pritesh Nath
         </h2>
       </div>
     </footer>
